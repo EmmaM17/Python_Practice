f4f6e9fb7fddeb1976dd39b69ede6ec78fbed05e
EmmaM17 <62438729+EmmaM17@users.noreply.github.com>
2023-07-20 15:01:26 +0000

    added First_Item()

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# 1) Min_2_Sec()
+# 2) Hours_2_Sec()
+# 3) First_item()
+# 4) Calculator()
+# 5) Number_2_Binary()
+
+
 #Minutes to seconds
 def Min_2_Sec():
   min = int(input("Please enter the number of minutes: "))
@@ -9,6 +16,13 @@ def Min_2_Sec():
 def Hours_2_Sec():
   hours = int(input("Please input the number of hours: "))
   print((hours * 60) * 60, " Seconds")
+
+
+#Get first item from a list
+def First_item():
+  lst = input("Please enter a list seperated by a comma: ")
+  lst = lst.split(",")
+  print(lst)
 
 
 #A basic calculator
@@ -28,3 +42,15 @@ def calculator():
   else:
     answer = 'error'
   print(answer)
+
+
+#Number to binary
+
+
+def Num_2_Binary():
+  number = int(input("Please enter a number: "))
+
+
+#class checkNumber
+#if number > 255:
+# print("error")
