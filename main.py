b86b95d7faee7c4b9ad0ed9cb15cf7fc0ada0331
EmmaM17 <62438729+EmmaM17@users.noreply.github.com>
2023-07-20 11:01:01 +0000

    Competed my first three tasks

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+#Minutes to seconds
+def Min_2_Sec():
+  min = int(input("Please enter the number of minutes: "))
+  sec = min * 60
+  print(sec, " Seconds")
 
+
+#Hours to seconds
+def Hours_2_Sec():
+  hours = int(input("Please input the number of hours: "))
+  print((hours * 60) * 60, " Seconds")
+
+
+#A basic calculator
+def calculator():
+  num1 = int(input("Please enter your first number: "))
+  operator = input("Please enter your operator (* / + -): ")
+  num2 = int(input("Please enter your second number: "))
+
+  if operator == '*':
+    answer = num1 * num2
+  elif operator == '/':
+    answer = num1 / num2
+  elif operator == '+':
+    answer = num1 + num2
+  elif operator == '-':
+    answer = num1 - num2
+  else:
+    answer = 'error'
+  print(answer)
